31ed5aa9f7d54919155f1992fc21a104af65186e
murizzi67 <murizzi.simone.23@itisriva.edu.it>
2026-04-11 15:56:00 +0200

    aggiunta menu regole e controlli di base

---
+++ RuotaDellaFortuna/Program.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RuotaDellaFortuna
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //interfaccia principale e menu
+            bool continua = true;
+            int scelta;
+            int Counter1stcheck = 0;
+            while (continua)
+            {
+                Menu();
+                Console.Write("Insersci numero --> ");
+                scelta = int.Parse(Console.ReadLine());
+
+                while (scelta < 0 || scelta > 4)
+                {
+                    
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.Write("!Input non valido!\n");
+                    if (Counter1stcheck > 1)
+                    {
+                        Console.WriteLine("!Deve essere compreso tra 0 e 3!");
+                    }
+                    Console.ResetColor();
+                    Console.Write("Insersci numero --> ");
+                    scelta = int.Parse(Console.ReadLine());
+                    Counter1stcheck++;
+                }
+                switch (scelta)
+                {
+                    case 1:
+                        //DEBUG: gico 
+                        break;
+                    case 2:
+                        Regole();
+                        break;
+                    case 3:
+                        //DEBUG: visualizzazione attivita nel turno
+                        break;
+                    case 0:
+                        continua = false;
+                        Console.WriteLine("Grazie per aver giocato!");
+                        break;
+                }
+            }
+        }
+        static void Menu() 
+        {
+            Console.ForegroundColor= ConsoleColor.Cyan;
+            Console.WriteLine("------------------------------");
+            Console.ResetColor();
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("1 - Gioca!");
+                Console.ResetColor();
+            Console.WriteLine("2 - Regole");
+            Console.WriteLine("3 - Visualizzazione attivita'");
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine("------------------------------");
+            Console.ResetColor();
+        }
+        static void Regole()
+        {
+            Console.Clear();
+
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("=== RUOTA DELLA FORTUNA - Versione Console ===\n");
+
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine("OBIETTIVO");
+            Console.ResetColor();
+            Console.WriteLine("Indovinare la frase nascosta accumulando piu soldi possibile.\n");
+
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine("LA FRASE");
+            Console.ResetColor();
+            Console.WriteLine("La frase e mostrata con _ al posto delle lettere non ancora scoperte.");
+            Console.WriteLine("Gli spazi sono visibili. Esempio:");
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("_ _ _ _   _ _ _ _ _ _\n");
+            Console.ResetColor();
+
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine("TURNO DI GIOCO");
+            Console.ResetColor();
+            Console.WriteLine("Ogni turno il giocatore sceglie tra tre azioni:");
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("  [1] Gira la ruota  -> valore casuale: 100, 200, 300, 500, 750, 1000, FALLIMENTO, BANCAROTTA");
+            Console.WriteLine("  [2] Compra una vocale -> costa 250, disponibile solo se hai almeno 250");
+            Console.WriteLine("  [3] Risolvi la frase  -> tenti di indovinare l'intera frase\n");
+            Console.ResetColor();
+
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine("DOPO AVER GIRATO LA RUOTA");
+            Console.ResetColor();
+            Console.WriteLine("  - Valore in denaro -> inserisci una consonante");
+            Console.WriteLine("      Se presente:  guadagni valore x numero di volte che appare");
+            Console.WriteLine("      Se assente:   perdi il turno");
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            Console.WriteLine("  - FALLIMENTO      -> passi il turno ma mantieni i soldi");
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("  - BANCAROTTA      -> perdi tutti i soldi e il turno\n");
+            Console.ResetColor();
+
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine("VOCALI E CONSONANTI");
+            Console.ResetColor();
+            Console.WriteLine("  - Vocali (A E I O U)  -> si comprano a 250");
+            Console.WriteLine("  - Consonanti          -> si ottengono girando la ruota\n");
+
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine("VITTORIA");
+            Console.ResetColor();
+            Console.WriteLine("Vinci quando risolvi correttamente la frase.");
+            Console.WriteLine("Il punteggio finale e il denaro accumulato.\n");
+
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine("GAME OVER");
+            Console.ResetColor();
+            Console.WriteLine("Se tutti i giocatori vanno in bancarotta e nessuno riesce a risolvere,");
+            Console.WriteLine("la frase viene rivelata e nessuno vince.\n");
+
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("Premi un tasto per continuare...");
+            Console.ResetColor();
+            Console.ReadKey();
+            Console.Clear();
+        }
+    }
+}
